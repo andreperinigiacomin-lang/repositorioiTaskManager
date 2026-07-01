f97e13af5ce586938d5679eff28925dd70b772d5
André Perini <andreperinigiacomin@gmail.com>
2026-07-01 11:28:25 -0300

    feat: adiciona a captura de hora e data em tempo real no formato HH/MM e  DD/MM/AAAA, respectiavmente

--- app.ts
+++ app.ts
@@ -1,3 +1,4 @@
+export{}
 interface Tarefa {
     titulo: string;
     descricao?: string;
@@ -9,4 +10,13 @@ const formTarefa = document.getElementById('form-tarefa') as HTMLFormElement;
 const inputTitulo = document.getElementById('titulo') as HTMLInputElement;
 const txtDescricao = document.getElementById('descricao') as HTMLTextAreaElement;
 const listaTarefas = document.getElementById('lista-tarefas') as HTMLDivElement;
-const contadorTarefas = document.getElementById('contador-tarefas') as HTMLSpanElement
+const contadorTarefas = document.getElementById('contador-tarefas') as HTMLSpanElement;
+
+let tarefas: Tarefa[] = [];
+
+function obterHora(): {data: string; hora: string}{
+    const agora = new Date()
+    const data = agora.toLocaleTimeString('pt-BR');
+    const hora = agora.toLocaleTimeString('pt-BR', { hour: '2-digit', minute: '2-digit'})
+    return {data, hora}
+}
