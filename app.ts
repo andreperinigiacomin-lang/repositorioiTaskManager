81dfc7279b03415161a1d516d73c51eeb34cb150
André Perini <andreperinigiacomin@gmail.com>
2026-07-01 19:37:42 -0300

    feat: adiciona a classe e constructor que vai pegar o que foi digitado no titulo e na descricao e vai joga-lo dentro de uma lista de tarefas. OBS.: adiciona sistemas de protecao para que o codigo nao quebre ou adicione titulos vazios

--- app.ts
+++ app.ts
@@ -15,37 +15,41 @@ class Tarefa {
         return this.dataCriacao.toLocaleDateString('pt-BR');
     }
     HoraFormatada(): string{
-        return this.dataCriacao.toLocaleDateString('pt-BR',{ hour: '2-digit', minute: '2-digit'})
+        return this.dataCriacao.toLocaleTimeString('pt-BR',{ hour: '2-digit', minute: '2-digit'})
     }
 }
+class GerenciadorTarefas {
+    listaDeTarefas: Tarefa[] = [];
+    inputTitulo = document.getElementById('titulo') as HTMLInputElement;
+    formTarefa = document.getElementById('form-tarefa') as HTMLFormElement;
+    txtDescricao = document.getElementById('descricao') as HTMLTextAreaElement;
+    listaTarefas = document.getElementById('lista-tarefas') as HTMLDivElement;
+    contadorTarefas = document.getElementById('contador-tarefas') as HTMLSpanElement;
 
+    constructor(){
+        this.inicializarEventos();
+    }
+    inicializarEventos(): void{
+        if(this.formTarefa){
+            this.formTarefa.addEventListener('submit', (event: SubmitEvent) => {
+                event.preventDefault();
+                this.adicionarNovaTarefa();
+            })
+        }
+    }
+    adicionarNovaTarefa(): void{
+        if(!this.inputTitulo || !this.txtDescricao || !this.formTarefa){
+            return;
+        }
+        const tituloDigitado = this.inputTitulo.value.trim();
+        const descricaoDigitada = this.txtDescricao.value.trim();
+        if (tituloDigitado === ''){
+            return;
+        }
+        const novaTarefa = new Tarefa(tituloDigitado, descricaoDigitada);
+        this.listaDeTarefas.push(novaTarefa);
+        //this.renderizar();
+        this.formTarefa.reset();
 
-
-
-//const formTarefa = document.getElementById('form-tarefa') as HTMLFormElement;
-//const inputTitulo = document.getElementById('titulo') as HTMLInputElement;
-//const txtDescricao = document.getElementById('descricao') as HTMLTextAreaElement;
-//const listaTarefas = document.getElementById('lista-tarefas') as HTMLDivElement;
-//const contadorTarefas = document.getElementById('contador-tarefas') as HTMLSpanElement;
-
-//let tarefas: Tarefa[] = [];
-
-//function obterHora(): {data: string; hora: string}{
-    //const agora = new Date()
-    //const data = agora.toLocaleTimeString('pt-BR');
-    //const hora = agora.toLocaleTimeString('pt-BR', { hour: '2-digit', minute: '2-digit'})
-    //return {data, hora}
-//}
-//function renderizarTela(): void{
-    //if(!listaTarefas||!contadorTarefas){
-        //return;
-   // }
-   // listaTarefas.innerHTML = '';
-    //tarefas.forEach((tarefa, index) => {
-       // const tarefaItem = document.createElement('div');
-       // tarefaItem.classList.add('tarefa-item');
-       // if (tarefa.concluida) {
-          //  tarefaItem.classList.add('concluida');
-       // }
-   // }
-//}
+    }
+}
